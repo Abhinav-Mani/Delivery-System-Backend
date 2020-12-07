b9390241163d32fa18b7914002d65cc27034e8ea
Abhinav-Mani <abhinav1946@gmail.com>
2020-12-07 17:11:47 +0530

    CORS added

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ dotenv.config()
 const port = process.env.PORT
 const app = express();
 
+const bodyParser = require("body-parser")
+
 const userRoute = require("./Routes/User");
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/user",userRoute);
 app.listen(port,()=>{
