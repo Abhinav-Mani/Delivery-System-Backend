cf9a17cbaf8f54b5556e7f3c1941e3746dac178c
Abhinav-Mani <abhinav1946@gmail.com>
2020-12-08 15:11:16 +0530

    Product route protected

--- Routes/Products.js
+++ Routes/Products.js
@@ -2,7 +2,10 @@ const route=require("express").Router();
 
 const ProductController = require("../Controllers/Products");
 
-route.post("/",ProductController.ADD);
+const authCheck = require("../util/AuthCheker");
+const adminCheck = require("../util/isAdmin");
+
+route.post("/",authCheck,adminCheck,ProductController.ADD);
 route.get("/", ProductController.GET);
 
 module.exports = route
